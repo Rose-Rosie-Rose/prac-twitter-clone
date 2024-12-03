27a92daf6e5798eced0c8020adabca86aff0f6c7
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-03 16:10:14 +0900

    feat: home 관련 기본 구조

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,138 @@
+import { FiImage } from "react-icons/fi";
+import { FaUserCircle } from "react-icons/fa";
+import { AiFillHeart } from "react-icons/ai";
+import { FaRegComment } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export interface PostProps {
+  id: string;
+  email: string;
+  content: string;
+  createdAt: string;
+  uid: string;
+  profileUrl?: string;
+  likeCount?: number;
+  comments?: any;
+}
+
+const posts: PostProps[] = [
+  {
+    id: "1",
+    email: "test@test.com",
+    content: "Hello, world!",
+    createdAt: "2022-01-01",
+    uid: "123123",
+  },
+  {
+    id: "2",
+    email: "test@test.com",
+    content: "Hello, world!",
+    createdAt: "2022-01-01",
+    uid: "123123",
+  },
+  {
+    id: "3",
+    email: "test@test.com",
+    content: "Hello, world!",
+    createdAt: "2022-01-01",
+    uid: "123123",
+  },
+  {
+    id: "4",
+    email: "test@test.com",
+    content: "Hello, world!",
+    createdAt: "2022-01-01",
+    uid: "123123",
+  },
+];
+
 export const HomePage = () => {
-  return <>Home Page</>;
+  const handleFileUpload = () => {};
+
+  const handleDelete = () => {};
+
+  return (
+    <div className="home">
+      <div className="hoome__title">Home</div>
+      <div className="home__tabs">
+        <div className="home__tab home__tab--active">For You</div>
+        <div className="home__tab">Following</div>
+      </div>
+      {/* Post form  */}
+      <form className="post-form">
+        <textarea
+          className="pst-form__textarea"
+          required
+          name="content"
+          id="content"
+          placeholder="What is happening?"
+        />
+        <div className="post-form__submit-area">
+          <label htmlFor="file-input" className="post-form__file">
+            <FiImage className="post-form__file-icon" />
+          </label>
+          <input
+            type="file"
+            name="file-input"
+            accept="image/*"
+            onChange={handleFileUpload}
+            className="hidden"
+          />
+          <input
+            type="submit"
+            value="Tweet"
+            className="post-form__submit-btn"
+          />
+        </div>
+      </form>
+      {/* Tweet post */}
+      <div className="post">
+        {posts?.map((post) => (
+          <div className="post__box" key={post?.id}>
+            <Link to={`/posts/${post?.id}`}>
+              <div className="post__box-profile">
+                <div className="post__flex">
+                  {post?.profileUrl ? (
+                    <img
+                      src={post?.profileUrl}
+                      alt="profile"
+                      className="post__box-profile-img"
+                    />
+                  ) : (
+                    <FaUserCircle className="post__box-profile-icon" />
+                  )}
+                  <div className="post__email">{post?.email}</div>
+                  <div className="post__createdAt">{post?.createdAt}</div>
+                </div>
+                <div className="post__box-content">{post?.content}</div>
+              </div>
+            </Link>
+            <div className="post__Box-footer">
+              {/* post.uid === user.uid 일 때 */}
+              <>
+                <button
+                  type="button"
+                  className="post__delete"
+                  onClick={handleDelete}
+                >
+                  Delete
+                </button>
+                <button type="button" className="post__edit">
+                  <Link to={`/posts/edit/${post?.id}`}>Edit</Link>
+                </button>
+              </>
+              <button type="button" className="post__likes">
+                <AiFillHeart />
+                {post?.likeCount || 0}
+              </button>
+              <button type="button" className="post__comments">
+                <FaRegComment />
+                {post?.comments?.length || 0}
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
