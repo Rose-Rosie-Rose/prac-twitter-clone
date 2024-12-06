cebee9aa44f132cf86751562e9093458f0e2402d
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-06 15:10:28 +0900

    feat: Router 사용자 인증 관련 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
 
   return (
     <Layout>
-      <Router />
+      <Router isAuthenticated={isAuthenticated} />
     </Layout>
   );
 };
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -13,21 +13,35 @@ import {
 import { PostNewPage } from "pages/posts/new";
 import { Navigate, Route, Routes } from "react-router-dom";
 
-export const Router = () => {
+interface RouterProps {
+  isAuthenticated: boolean;
+}
+
+export const Router = ({ isAuthenticated }: RouterProps) => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/posts" element={<PostListPage />} />
-      <Route path="/posts/:id" element={<PostDetailPage />} />
-      <Route path="/posts/new" element={<PostNewPage />} />
-      <Route path="/posts/edit/:id" element={<PostEditPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/profile/edit" element={<ProfileEditPage />} />
-      <Route path="/notifications" element={<NotificationsPage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/users/login" element={<LoginPage />} />
-      <Route path="/users/signup" element={<SignUpPage />} />
-      <Route path="*" element={<Navigate replace to="/" />} />
+      {isAuthenticated ? (
+        <>
+          {" "}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/posts" element={<PostListPage />} />
+          <Route path="/posts/:id" element={<PostDetailPage />} />
+          <Route path="/posts/new" element={<PostNewPage />} />
+          <Route path="/posts/edit/:id" element={<PostEditPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile/edit" element={<ProfileEditPage />} />
+          <Route path="/notifications" element={<NotificationsPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </>
+      ) : (
+        <>
+          {" "}
+          <Route path="/users/login" element={<LoginPage />} />
+          <Route path="/users/signup" element={<SignUpPage />} />
+          <Route path="*" element={<Navigate replace to="/users/login" />} />
+        </>
+      )}
     </Routes>
   );
 };
