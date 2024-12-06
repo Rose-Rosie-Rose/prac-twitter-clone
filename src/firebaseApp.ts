a6a56106792f5ecd8f414a4f374b4466fde70fc1
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-06 14:46:20 +0900

    build: firebase 설치

---
+++ src/firebaseApp.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRyd7NIFzDOuKwR9afLTvu1d7yK61ICss",
+  authDomain: "prac-twitter-clone.firebaseapp.com",
+  projectId: "prac-twitter-clone",
+  storageBucket: "prac-twitter-clone.firebasestorage.app",
+  messagingSenderId: "593132628700",
+  appId: "1:593132628700:web:0e60fbc1853bdca891e219"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
