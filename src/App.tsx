4e4b9ffbd9c4b923756c44019931b16b84e395f3
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-06 15:05:29 +0900

    docs: 사용자 인증 관련 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { Layout, Router } from "components";
+import { getAuth } from "firebase/auth";
+import { app } from "firebaseApp";
+import { useState } from "react";
 
 const App = () => {
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
   return (
     <Layout>
       <Router />
