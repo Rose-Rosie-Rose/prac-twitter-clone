dbf07193b13f21fa54908d8ea9a5b44c398af154
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-10 20:50:47 +0900

    docs: tostify 기본 작업

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { getAuth } from "firebase/auth";
 import { app } from "firebaseApp";
 import { useState } from "react";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   const auth = getAuth(app);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
@@ -11,6 +14,7 @@ const App = () => {
 
   return (
     <Layout>
+      <ToastContainer />
       <Router isAuthenticated={isAuthenticated} />
     </Layout>
   );
