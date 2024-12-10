bcbedb36c9c9bba727d0a59454a7b11db8eca5f2
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-10 20:48:40 +0900

    docs: sign up form 기능 작업중

--- src/components/users/SignUpForm.tsx
+++ src/components/users/SignUpForm.tsx
@@ -1,16 +1,93 @@
-import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { app } from "firebaseApp";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export const SignUpForm = () => {
+  const [error, setError] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState<string>("");
+  const navigate = useNavigate();
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth(app);
+      await createUserWithEmailAndPassword(auth, email, password);
+
+      navigate("/");
+    } catch (error: any) {}
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "email") {
+      setEmail(value);
+
+      const validRegex =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+      if (!value?.match(validRegex)) {
+        setError("이메일 형식이 올바르지 않습니다.");
+      } else {
+        setError("");
+      }
+    }
+
+    if (name === "password") {
+      setPassword(value);
+
+      if (value?.length < 8) {
+        setError("비밀번호는 8자리 이상 입력해주세요.");
+      } else if (value !== passwordConfirmation) {
+        setError("비밀번호와 비밀번호 확인 값이 다릅니다.");
+      } else {
+        setError("");
+      }
+    }
+
+    if (name === "password__confirmation") {
+      setPasswordConfirmation(value);
+
+      if (value?.length < 8) {
+        setError("비밀번호는 8자리 이상 입력해주세요.");
+      } else if (value !== password) {
+        setError("비밀번호와 비밀번호 확인 값이 다릅니다.");
+      } else {
+        setError("");
+      }
+    }
+  };
+
   return (
-    <form className="form form--lg">
+    <form className="form form--lg" onSubmit={onSubmit}>
       <div className="form__title">회원가입</div>
       <div className="form__block">
         <label className="email">이메일</label>
-        <input type="text" name="email" id="email" required />
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          required
+          onChange={onChange}
+        />
       </div>
       <div className="form__block">
         <label className="password">비밀번호</label>
-        <input type="password" name="password" id="password" required />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          required
+          onChange={onChange}
+        />
       </div>
       <div className="form__block">
         <label className="password__confirmation">비밀번호 확인</label>
@@ -18,12 +95,16 @@ export const SignUpForm = () => {
           type="password"
           name="password__confirmation"
           id="password__confirmation"
+          value={passwordConfirmation}
           required
+          onChange={onChange}
         />
       </div>
-      <div className="form__block">
-        <div className="form__error">{}</div>
-      </div>
+      {error && error?.length > 0 && (
+        <div className="form__block">
+          <div className="form__error">{error}</div>
+        </div>
+      )}
       <div className="form__block">
         계정이 있으신가요?
         <Link to="/login" className="form__link">
@@ -31,7 +112,11 @@ export const SignUpForm = () => {
         </Link>
       </div>
       <div className="form__block">
-        <button type="submit" className="form__btn-submit">
+        <button
+          type="submit"
+          className="form__btn-submit"
+          disabled={error?.length > 0}
+        >
           회원가입
         </button>
       </div>
