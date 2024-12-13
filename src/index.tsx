05070fdc61df16ab8c682590619e16533d897437
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-13 16:24:54 +0900

    docs: AuthContext 및 menu 관련 작업

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "context/AuthContext";
+import { AuthContextProvider } from "context";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
