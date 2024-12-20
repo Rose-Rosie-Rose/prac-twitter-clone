8a7f0dbb110214cf8f6a909bf81f1be0f4e13887
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-20 16:20:36 +0900

    docs: post detail 페이지 생성

--- src/pages/posts/detail/index.tsx
+++ src/pages/posts/detail/index.tsx
@@ -1,3 +1,11 @@
+import { Loader, PostBox } from "components";
+import { PostProps } from "pages/home";
+import { useState } from "react";
+
 export const PostDetailPage = () => {
-  return <>Post Detail Page</>;
+  const [post, setPost] = useState<PostProps | null>(null);
+
+  return (
+    <div className="post">{post ? <PostBox post={post} /> : <Loader />}</div>
+  );
 };
