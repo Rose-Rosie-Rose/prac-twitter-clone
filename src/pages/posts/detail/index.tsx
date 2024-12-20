0f039d5ba02f49f9729e2735d3c813808fb30845
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-20 16:25:19 +0900

    feat: post detail 페이지 데이터 불러오기 연동

--- src/pages/posts/detail/index.tsx
+++ src/pages/posts/detail/index.tsx
@@ -1,9 +1,26 @@
 import { Loader, PostBox } from "components";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
 import { PostProps } from "pages/home";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export const PostDetailPage = () => {
+  const params = useParams();
   const [post, setPost] = useState<PostProps | null>(null);
+
+  const getPost = useCallback(async () => {
+    if (params.id) {
+      const docRef = doc(db, "posts", params.id);
+      const docSnap = await getDoc(docRef);
+
+      setPost({ ...(docSnap?.data() as PostProps), id: docSnap?.id });
+    }
+  }, [params.id]);
+
+  useEffect(() => {
+    if (params.id) getPost();
+  }, [getPost, params.id]);
 
   return (
     <div className="post">{post ? <PostBox post={post} /> : <Loader />}</div>
