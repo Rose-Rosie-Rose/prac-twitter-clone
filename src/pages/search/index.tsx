402b9d38d8818297909670d4af22e09ae8207fd3
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-26 14:02:24 +0900

    feat: search api 연결

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,14 +1,44 @@
 import { PostBox } from "components";
+import { AuthContext } from "context";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "firebaseApp";
 import { PostProps } from "pages/home";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export const SearchPage = () => {
   const [posts, setPosts] = useState<PostProps[]>([]);
   const [tagQuery, setTagQuery] = useState<string>("");
+  const { user } = useContext(AuthContext);
 
   const handleOnChange = (e: any) => {
     setTagQuery(e?.target?.value?.trim());
   };
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, "posts");
+      let postsQuery = query(
+        postsRef,
+        where("hashTags", "array-contains-any", [tagQuery]),
+        orderBy("createdAt", "desc")
+      );
+
+      onSnapshot(postsQuery, (snapshot) => {
+        let dataObj = snapshot?.docs?.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [tagQuery, user]);
 
   return (
     <div className="home">
