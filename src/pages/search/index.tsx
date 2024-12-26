c8d3b9bc8c80a9c79be6a24d0dce66ee7c9c9b13
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-26 13:51:44 +0900

    docs: search page 기본 틀 작성

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,3 +1,27 @@
+import { PostBox } from "components";
+
 export const SearchPage = () => {
-  return <>Search Page</>;
+  return (
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">
+          <div className="home__title-text">Search</div>
+        </div>
+        <div className="home__search-div">
+          <input className="home__search" placeholder="해시태그 검색" />
+        </div>
+      </div>
+      <div className="post">
+        <div className="post">
+          {posts?.length > 0 ? (
+            posts?.map((post) => <PostBox post={post} key={post.id} />)
+          ) : (
+            <div className="post__no--posts">
+              <div className="post__text">게시글이 없습니다.</div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
