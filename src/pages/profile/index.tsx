e01ccc2f2d349a5ccbc609d5839a78392ada94e2
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2025-01-03 16:05:49 +0900

    feat: 프로필 페이지 기본 작업

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,3 +1,57 @@
+import { PostBox } from "components";
+import { AuthContext } from "context";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "firebaseApp";
+import { PostProps } from "pages/home";
+import { useContext, useEffect, useState } from "react";
+
 export const ProfilePage = () => {
-  return <>Profile Page</>;
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, "posts");
+      let postsQuery = query(
+        postsRef,
+        where("uid", "==", user.uid),
+        orderBy("createdAt", "desc")
+      );
+
+      onSnapshot(postsQuery, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [user]);
+
+  return (
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">Profile</div>
+        <div className="home__tabs">
+          <div className="home__tab home__tab--active">For You</div>
+          <div className="home__tab">Likes</div>
+        </div>
+        <div className="post">
+          {posts?.length > 0 ? (
+            posts?.map((post) => <PostBox post={post} key={post.id} />)
+          ) : (
+            <div className="post__no-posts">
+              <div className="post__text">게시글이 없습니다.</div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
