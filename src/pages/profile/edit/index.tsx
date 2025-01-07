7eaeb85bb6067679472a2a432b6d6d784303af54
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2025-01-07 17:19:02 +0900

    feat: 프로필 수정 api 연동

--- src/pages/profile/edit/index.tsx
+++ src/pages/profile/edit/index.tsx
@@ -1,14 +1,77 @@
-import { PostHeader } from "components";
-import { AuthContext } from "context";
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { FiImage } from "react-icons/fi";
+import { AuthContext } from "context/AuthContext";
+import {
+  ref,
+  deleteObject,
+  uploadString,
+  getDownloadURL,
+} from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+import { v4 as uuidv4 } from "uuid";
+import { storage } from "firebaseApp";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { PostHeader } from "components";
+
+const STORAGE_DOWNLOAD_URL_STR = "https://firebasestorage.googleapis.com";
 
 export const ProfileEditPage = () => {
   const [displayName, setDisplayName] = useState<string>("");
   const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const navigate = useNavigate();
   const { user } = useContext(AuthContext);
 
-  const HandleOnChange = () => {};
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+
+    setDisplayName(value);
+  };
+
+  const onSubmit = async (e: any) => {
+    let key = `${user?.uid}/${uuidv4()}`;
+    const storageRef = ref(storage, key);
+    let newImageUrl = null;
+
+    e.preventDefault();
+
+    try {
+      if (
+        user?.photoURL &&
+        user?.photoURL?.includes(STORAGE_DOWNLOAD_URL_STR)
+      ) {
+        const imageRef = ref(storage, user?.photoURL);
+        if (imageRef) {
+          await deleteObject(imageRef).catch((error) => {
+            console.log(error);
+          });
+        }
+      }
+
+      if (imageUrl) {
+        const data = await uploadString(storageRef, imageUrl, "data_url");
+        newImageUrl = await getDownloadURL(data?.ref);
+      }
+
+      if (user) {
+        await updateProfile(user, {
+          displayName: displayName || "",
+          photoURL: newImageUrl || "",
+        })
+          .then(() => {
+            toast.success("프로필이 업데이트 되었습니다.");
+            navigate("/profile");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    } catch (e: any) {
+      console.log(e);
+    }
+  };
 
   const handleFileUpload = (e: any) => {
     const {
@@ -18,7 +81,6 @@ export const ProfileEditPage = () => {
     const file = files?.[0];
     const fileReader = new FileReader();
     fileReader.readAsDataURL(file);
-
     fileReader.onloadend = (e: any) => {
       const { result } = e?.currentTarget;
       setImageUrl(result);
@@ -30,23 +92,25 @@ export const ProfileEditPage = () => {
   };
 
   useEffect(() => {
-    console.log(user);
     if (user?.photoURL) {
       setImageUrl(user?.photoURL);
     }
-  }, [user?.photoURL]);
+    if (user?.displayName) {
+      setDisplayName(user?.displayName);
+    }
+  }, [user?.displayName, user?.photoURL]);
 
   return (
     <div className="post">
       <PostHeader />
-      <form className="post-form">
+      <form className="post-form" onSubmit={onSubmit}>
         <div className="post-form__profile">
           <input
             type="text"
             name="displayName"
             className="post-form__input"
             placeholder="이름"
-            onChange={HandleOnChange}
+            onChange={onChange}
             value={displayName}
           />
           {imageUrl && (
