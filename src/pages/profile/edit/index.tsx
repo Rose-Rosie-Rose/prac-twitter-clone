1323eb8789b29590978abed15229c64534420581
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2025-01-07 16:25:00 +0900

    feat: 프로필 수정 기본 form 작성

--- src/pages/profile/edit/index.tsx
+++ src/pages/profile/edit/index.tsx
@@ -1,3 +1,48 @@
+import { PostHeader } from "components";
+import { useState } from "react";
+import { FiImage } from "react-icons/fi";
+
 export const ProfileEditPage = () => {
-  return <>Profile Edit Page</>;
+  const [displayName, setDisplayName] = useState<string>("");
+  const HandleOnChange = () => {};
+
+  const handleFileUpload = () => {};
+
+  return (
+    <div className="post">
+      <PostHeader />
+      <form className="post-form">
+        <div className="post-form__profile">
+          <input
+            type="text"
+            name="displayName"
+            className="post-form__input"
+            placeholder="이름"
+            onChange={HandleOnChange}
+            value={displayName}
+          />
+          <div className="post-form__submit-area">
+            <div className="post-form__image-area">
+              <label className="post-form__file" htmlFor="file-input">
+                <FiImage className="post-form__file-icon" />
+              </label>
+            </div>
+            <input
+              type="file"
+              name="file-input"
+              id="file-input"
+              accept="image/*"
+              className="hidden"
+              onChange={handleFileUpload}
+            />
+            <input
+              type="submit"
+              value="프로필 수정"
+              className="post-form__submit-btn"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
