99becf43b71d9b29bc1cea5ace85c634aa12aea9
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2025-01-14 15:52:34 +0900

    feat: teat 입력관련 change 함수 작성

--- src/components/comments/CommentForm.tsx
+++ src/components/comments/CommentForm.tsx
@@ -1,18 +1,35 @@
 import { PostProps } from "pages";
+import { useState } from "react";
 
 export interface CommentProps {
   post: PostProps | null;
 }
 
 export const CommentForm = ({ post }: CommentProps) => {
+  const [comment, setComment] = useState<string>("");
+
+  const onSubmitHandler = () => {};
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "comment") {
+      setComment(value);
+    }
+  };
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={onSubmitHandler}>
       <textarea
         className="post-form__textarea"
         name="comment"
         id="comment"
         required
         placeholder="What is happening?"
+        onChange={onChangeHandler}
+        value={comment}
       />
       <div className="post-form__submit-area">
         <div />
@@ -20,6 +37,7 @@ export const CommentForm = ({ post }: CommentProps) => {
           type="submit"
           value="comment"
           className="post-form__submit-btn"
+          disabled={!comment}
         />
       </div>
     </form>
