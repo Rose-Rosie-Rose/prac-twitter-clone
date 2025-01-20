68e29ab6852622de4646567d3a0ed239529e9280
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2025-01-20 16:35:24 +0900

    feat: 알림 관련 api 연동 및 불러오기

--- src/pages/notifications/index.tsx
+++ src/pages/notifications/index.tsx
@@ -1,3 +1,46 @@
+import { AuthContext } from "context";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "firebaseApp";
+import { useContext, useEffect, useState } from "react";
+
+export interface NotificationProps {
+  id: string;
+  uid: string;
+  url: string;
+  isRead: string;
+  content: string;
+  createdAt: string;
+}
+
 export const NotificationsPage = () => {
+  const { user } = useContext(AuthContext);
+  const [notifications, setNotifications] = useState<NotificationProps[]>([]);
+
+  useEffect(() => {
+    if (user) {
+      let ref = collection(db, "notifications");
+      let notificationQuery = query(
+        ref,
+        where("uid", "==", user?.uid),
+        orderBy("createdAt", "desc")
+      );
+
+      onSnapshot(notificationQuery, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+
+        setNotifications(dataObj as NotificationProps[]);
+      });
+    }
+  }, [user]);
+
   return <div></div>;
 };
